7d1276ed4a945168130b20911825a8b71d5b7f4f
Alejo Abrate <abratealejo@gmail.com>
2026-04-27 16:36:14 -0300

    Add files via upload

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parcial1_Prog3.Data
+{
+    public class AppDBContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-MA09TKJ\\SQLEXPRESS;Database=Parcial1_Prog3;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Models.Producto> Productos { get; set; }
+
+
+
+    }
+}
